dcc835bd4751f8a6f6cf74234ceaf9c573e19474
Andrei-Alexandru <119253073+AndreiMortu@users.noreply.github.com>
2023-05-07 23:41:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,91 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-void rank_pairing_heap(vector<int>& arr) {
-    vector<vector<int>> tree;
-    tree.push_back(arr);
-    int n = tree.size();
+// Implementation of rank pairing heap method
+void rank_pairing_heap_sort(vector<int>& arr) {
+    if (arr.size() <= 1) return;
 
-    for (int i = n-1; i > 0; i--) {
-        if (tree[i].size() < tree[i-1].size()) {
-            swap(tree[i], tree[i-1]);
-        }
-        tree[i].push_back(tree[i-1].back());
-        tree[i-1].pop_back();
-        if (tree[i].size() == tree[i-1].size() + 2) {
-            tree[i-1].push_back(tree[i].back());
-            tree[i].pop_back();
-        }
-        while (i > 1 && tree[i].size() == tree[i-2].size()) {
-            vector<int> temp;
-            temp.insert(temp.end(), tree[i-2].begin(), tree[i-2].end());
-            temp.insert(temp.end(), tree[i].begin(), tree[i].end());
-            sort(temp.begin(), temp.end());
-            tree[i-2] = temp;
-            tree.erase(tree.begin() + i - 1, tree.begin() + i + 1);
-            i -= 2;
-        }
+    // Create the initial forest of trees
+    vector<vector<int>> forest;
+    for (int i = 0; i < arr.size(); i++) {
+        forest.push_back(vector<int>{arr[i]});
     }
 
-    arr = tree[0];
+    // Repeatedly merge pairs of trees until only one tree remains
+    while (forest.size() > 1) {
+        vector<vector<int>> new_forest;
+        for (int i = 0; i < forest.size(); i += 2) {
+            if (i == forest.size() - 1) {
+                // If there's an odd number of trees, add the last one to the new forest
+                new_forest.push_back(forest[i]);
+            } else {
+                // Merge the two trees and add the result to the new forest
+                vector<int> merged_tree;
+                int j = 0, k = 0;
+                while (j < forest[i].size() && k < forest[i + 1].size()) {
+                    if (forest[i][j] < forest[i + 1][k]) {
+                        merged_tree.push_back(forest[i][j]);
+                        j++;
+                    } else {
+                        merged_tree.push_back(forest[i + 1][k]);
+                        k++;
+                    }
+                }
+                while (j < forest[i].size()) {
+                    merged_tree.push_back(forest[i][j]);
+                    j++;
+                }
+                while (k < forest[i + 1].size()) {
+                    merged_tree.push_back(forest[i + 1][k]);
+                    k++;
+                }
+                new_forest.push_back(merged_tree);
+            }
+        }
+        forest = new_forest;
+    }
+
+    // Copy the sorted array back into the original array
+    arr = forest[0];
 }
 
 int main() {
-    ifstream infile("input.txt");
+    // Read input from file
+    ifstream input_file("input.txt");
     int n;
-    infile >> n;
+    input_file >> n;
 
-    vector<vector<int>> arrays(n);
+    vector<vector<int>> arrays;
     for (int i = 0; i < n; i++) {
-        int x;
-        while (infile >> x) {
-            arrays[i].push_back(x);
+        vector<int> arr;
+        int len;
+        input_file >> len;
+        for (int j = 0; j < len; j++) {
+            int x;
+            input_file >> x;
+            arr.push_back(x);
         }
+        arrays.push_back(arr);
+    }
+    input_file.close();
+
+    // Sort the arrays using rank pairing heap method
+    for (int i = 0; i < n; i++) {
+        rank_pairing_heap_sort(arrays[i]);
     }
 
-    for (auto& arr : arrays) {
-        rank_pairing_heap(arr);
-        for (int x : arr) {
-            cout << x << " ";
+    // Write sorted arrays to output file
+    ofstream output_file("output.txt");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < arrays[i].size(); j++) {
+            output_file << arrays[i][j] << " ";
         }
-        cout << endl;
+        output_file << endl;
     }
+    output_file.close();
 
     return 0;
 }
